a5dff4e1e6ba7c00a53dd7c4b46bfe0b58ddc375
butterbeetle <butterbeetle@naver.com>
2022-06-19 23:05:18 +0900

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("Call an api");
+  console.log("I run all the time.");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  }
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
