334515d55e1585f1cae139344357c36abd53ebd2
butterbeetle <butterbeetle@naver.com>
2022-06-21 00:07:37 +0900

    7.2 Coin Tracker & code Challenge

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [myUSD, setMyUSD] = useState(0);
+  const onChange_USD = (event) => {
+    //console.log(event.target.value);
+    setMyUSD(event.target.value);
+  }
 
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") return;
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const calc = (usd, coinValue) => {
+    //console.log(usd, coinValue);
+    if (coinValue === 0 || usd === 0) {
+      return 0
+    }
+    else {
+      return Math.floor(usd / coinValue);
+    }
+  }
+
+  const [buyCoin, setBuyCoin] = useState(0);
+  const onChange_Select = (event) => {
+    //console.log(event.target.value);
+    setBuyCoin(event.target.value);
+  }
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          text="text"
-          placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange_USD}
+        type="number"
+        placeholder="Write Your USD" /> USD
+      <br />
+      {loading ? (<strong>Loading...</strong>) : (
+        <select onChange={onChange_Select}>
+          {coins.map((coin) => (
+            <option
+              key={coin.id}
+              value={(coin.quotes.USD.price).toFixed(3)}>
+              {coin.name} ({coin.symbol}): ${(coin.quotes.USD.price).toFixed(3)} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <h3>You can buy {calc(myUSD, buyCoin)} Coin</h3>
     </div>
   );
 }
