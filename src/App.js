ca5e5a8859663c30ee5fa89c68807d32530c2c93
butterbeetle <butterbeetle@naver.com>
2022-06-20 22:33:11 +0900

    7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { useState, useEffect, useSyncExternalStore } from "react";
-
-function Hello() {
-
-  /* #type 1 */
-  useEffect(() => {
-    console.log("Hi");
-    return () => console.log("Bye~");
-  }, []);
-
-  /* #type 2 */
-  useEffect(function () {
-    console.log("Hi");
-    return function byeFn() {
-      console.log("Bye~");
-    }
-  }, []);
-
-  return <h1>Hello</h1>
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          text="text"
+          placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
